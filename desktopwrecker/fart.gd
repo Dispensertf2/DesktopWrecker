385013adc8582f82cbc68d30af3591cce5627993
Dispenser <72995022+Dispensertf2@users.noreply.github.com>
2022-04-27 17:29:56 -0400

    didnt really do anything lol

---
+++ desktopwrecker/fart.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print(ProjectSettings.get_setting("display/window/size/width"))
+
